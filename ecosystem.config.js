e9759e1047d53677d3f15172796f70fce73a01b0
pyliu <3634168+pyliu@users.noreply.github.com>
2022-03-01 21:46:34 +0800

    add ecosystem.config.js for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [{
+    name: 'LAH-WSS',
+    exec_mode: 'cluster',
+    instances: 1,
+    script: './server.js',
+    args: 'ws',
+    out_file: './wss_out.log',
+    error_file: './wss_err.log',
+    cron_restart: '0 7 * * *',
+    time: true,
+    watch: true,
+    ignore_watch: ['[/\\]./', 'node_modules', '*.bat', '.git', './db/*'],
+    max_memory_restart: '256M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    wait_ready: true,
+    restart_delay: 5000
+  }]
+}
